81e289de4c26fc68783008528963edad8e7655a3
Nuchit Atjanawat <nuchit@outlook.com>
2022-02-21 11:22:11 +0700

    initail project

---
+++ src/main/java/com/janawatECommerceSystem/janawatECommerceSystem/JanawatECommerceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.janawatECommerceSystem.janawatECommerceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanawatECommerceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanawatECommerceSystemApplication.class, args);
+	}
+
+}
